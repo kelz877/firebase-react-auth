f193c4e0b120f3abff51a33cc12f44bc3fabe8b4
kelly benson <kellyjbenson@gmail.com>
2019-10-04 08:04:13 -0500

    adding .env file

---
+++ client/src/components/firebase/Firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAI,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGE_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
+};
